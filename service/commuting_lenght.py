2a2e10dafc62cd722e9d9bb4a868c337a2c84014
rlario <rlario@paradigmatecnologico.com>
2012-12-26 00:28:11 +0100

    New distance calculation, new normalization (amount of dynamic users) NB: input TSV & output TSV, SHPs in the same location than this .py

--- service/commuting_lenght.py
+++ service/commuting_lenght.py
@@ -21,6 +21,44 @@ def __get_collection(config):
     collection = db[config['db']['collection']]
     return collection
     
+
+def distance_on_unit_sphere(x,y):
+    """ http://www.johndcook.com/python_longitude_latitude.html """
+    
+    if x[0] == -1 or y[0] ==-1:
+        return 0.0
+
+    # Convert latitude and longitude to 
+    # spherical coordinates in radians.
+    degrees_to_radians = math.pi/180.0
+        
+    # phi = 90 - latitude
+    phi1 = (90.0 - x[1])*degrees_to_radians
+    phi2 = (90.0 - y[1])*degrees_to_radians
+        
+    # theta = longitude
+    theta1 = x[0]*degrees_to_radians
+    theta2 = y[0]*degrees_to_radians
+        
+    # Compute spherical distance from spherical coordinates.
+        
+    # For two locations in spherical coordinates 
+    # (1, theta, phi) and (1, theta, phi)
+    # cosine( arc length ) = 
+    #    sin phi sin phi' cos(theta-theta') + cos phi cos phi'
+    # distance = rho * arc length
+    
+    cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) + 
+           math.cos(phi1)*math.cos(phi2))
+    if cos > 1: # sometimes, numerical operations return a cos  1.00000002 > 1 math domain error !!
+        cos = 1
+    arc = math.acos( cos )
+
+    # Remember to multiply arc by the radius of the earth 
+    # in your favorite set of units to get length.
+    return arc * 6373 # km
+
+
     
 def get_distance(x,y):
     """
@@ -32,9 +70,9 @@ def get_distance(x,y):
     else:
         return math.sqrt(math.pow((x[0]-y[0]),2) + math.pow((x[1]-y[1]),2))
         
-# Load atenna positions
+# Load antenna positions
 antennas = {}
-for line in open("../rawdata/ANT_POS.TSV" , 'r'):
+for line in open("ANT_POS.TSV" , 'r'):
     line = line.replace("\n","")
     chunks = line.split("\t")
     if len(chunks) == 3:
@@ -47,50 +85,66 @@ day = 3
 month = 4
 year = 2012
 
-f_out = open("/tmp/output-%s_%s_%s.tsv" % (day, month, year), "w")
+f_out = open("output-%s_%s_%s.tsv" % (day, month, year), "w")
 # Statistic vars
 ci_values = []
 print("%s\t%s\t%s" % ("hour", "lenght", "total"))
 f_out.write("%s\t%s\t%s\n" % ("hour", "lenght", "total"))
 
-# Perform analysis for each day
-for h in range(0,23):
+# Perform analysis for each day -- interval [)
+for h in range(0,24): # 1h step
     start = datetime(year, month, day, h, 0, 0)
+
     end =   datetime(year, month, day, h, 59, 59)
+#for h in range(0,48): # 30min step
+#    if h % 2: # 00min 00sec -> 29min 59sec
+#        start = datetime(year, month, day, int(h/2), 0, 0)
+#        end =   datetime(year, month, day, int(h/2), 29, 59)
+#    else: # 30min 00sec -> 59min 59sec
+#        start = datetime(year, month, day, int(h/2), 30, 0)
+#        end =   datetime(year, month, day, int(h/2), 59, 59)
+
+    
     # Debug info
     #print(start," ",end)  
+    
     # Create user traces              
     users = {}
     for trace in __get_collection(config).find({'date': {'$gte': start, '$lt':end}}):
         if trace["userid"] not in users:
             users[trace["userid"]] = []
         users[trace["userid"]].append(trace["antennaid"])
+    
     # Calculate max length and user distance
     antenna_cont = 0
+    dynamic_users_cont = 0 # only those ones who really change their location during this hour
     users_final = {}
     for user in users:
         users_final[user] = {}
         users_final[user]["trace_points"] = []
-        n = len(users[user])
+        n = len(users[user]) # amount of calls <-> antennas for this customer and this hour
         i = 0
         acum = 0
-        points = []
-        while i < n - 1:
+        while i < n - 1: # since each iteration is for i and i+1
             if antennas[users[user][i]][0] != -1 and antennas[users[user][i+1]][0] != -1:
                 users_final[user]["trace_points"].append(antennas[users[user][i]])
-                acum += get_distance(antennas[users[user][i]], antennas[users[user][i+1]])
+                acum += distance_on_unit_sphere(antennas[users[user][i]], antennas[users[user][i+1]])
+#                acum += get_distance(antennas[users[user][i]], antennas[users[user][i+1]])
                 antenna_cont += 1
             i += 1
         users_final[user]["trace_lenght"] = acum
+        if acum > 0:
+            dynamic_users_cont += 1
     # Return the mean
+    acum = 0
     for user in users_final:
         acum += users_final[user]["trace_lenght"]
     try:
         # Save max and min commuting index
-        current_ci = acum/antenna_cont
+        current_ci = acum/dynamic_users_cont
         ci_values.append(current_ci)
         # Dump information
-        print("%s\t%s\t%s" % (h, current_ci, antenna_cont))
+        print("%s\t%s\t%s" % (h, current_ci, dynamic_users_cont))
         f_out.write("%s\t%s\t%s\n" % (h, current_ci, antenna_cont))
     except:
         print("%s\t%s\t%s" % (h, 0, 0))
@@ -141,8 +195,8 @@ for antenna in antenna_weight:
     w_traces_short.record(WEIGHT = math.log(antenna_weight[antenna]))
     
 # Save shapefiles
-w_traces_long.save('/tmp/cl_long.shp')
-w_traces_short.save('/tmp/cl_short.shp')
+w_traces_long.save('cl_long.shp')
+w_traces_short.save('cl_short.shp')
 
 
 
