b8bdce33073385f5a6b07d760f26b954cc2c4d11
Mario Muñoz <mariomgal@gmail.com>
2013-01-29 09:03:58 +0100

    Minor fixes

--- service/map_data_provider.py
+++ service/map_data_provider.py
@@ -14,19 +14,19 @@ for day in days:
     for hour in hours:
         logging.info("Storing data in memory for [%s][%s]" % (day, hour))
         try:
-            with(open('%s_hour_%s.tsv' % (day, hour),'r')) as data_file:
+            with(open('../antennas_weight/%s_hour_%s.tsv' % (day, hour),'r')) as data_file:
                 for line in data_file.readlines():
                     line = line.replace('\n', '')
                     fields = line.split('\t')
-                    point = {'coordinates':[float(fields[0]), float(fields[1])], 
-                             'weight': float(fields[2])}
-                    data[day][str(hour)].append(point)
-        except ValueError as e:
-            pass
+                    try:
+                        point = {'coordinates':[float(fields[0]), float(fields[1])], 
+                                 'weight': float(fields[2])}
+                        data[day][str(hour)].append(point)
+                    except ValueError as e:
+                        pass
         except Exception as e:
             logging.error("The file %s_hour_%s.tsv does not exist" % (day, hour))
 
 def get_data(day, hour):
-    print(data.keys())
-    print(data['L'].keys())
     return data.get(day, {}).get(hour, {})
+
