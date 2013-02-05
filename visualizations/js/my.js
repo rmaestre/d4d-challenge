2b49457f60239f766e0ebbc1c0b3d50e9ea557d6
Julio M. Gonzalez <jmgonzalez@alumnos.upm.es>
2013-02-05 11:07:26 +0100

    New Version using leaflet and with first functionalities

--- visualizations/js/my.js
+++ visualizations/js/my.js
@@ -1,11 +1,14 @@
 $(function() {
-    var map = L.map('map').setView([7.56944, -5.33936], 7);
-    map.setMaxBounds(new L.LatLngBounds(new L.LatLng(14.08138, -12.26074),new L.LatLng(0.66693, 2.02148)));
+    var map = L.map('map', { zoomControl:true });
+	map.setMaxBounds(new L.LatLngBounds(new L.LatLng(14.08138, -12.26074),new L.LatLng(0.66693, 2.02148)));
     L.tileLayer('http://176.9.70.241:7890/d4d/{z}/{x}/{y}.png', {
     	minZoom: 6,
     	maxZoom: 13,
+		attribution: 'Developped by <a href="http://labs.paradigmatecnologico.com/about"><img style="height:18px;" src="http://labs.paradigmatecnologico.com/wordpress/wp-content/uploads/2011/03/paradigmaLabV31.png"></a> with cooperation of <a href="http://www.csic.es">CSIC</a>'
     }).addTo(map);
-    var popup = L.popup();
+    map.setView([7.56944, -5.33936], 7);
+
+	var popup = L.popup();
     function onMapClick(e) {
         popup
             .setLatLng(e.latlng)
@@ -13,4 +16,19 @@ $(function() {
             .openOn(map);
     }
     map.on('click', onMapClick);
+	
+
+	// create fullscreen control
+	var fullScreen = new L.Control.FullScreen();
+	// add fullscreen control to the map
+	map.addControl(fullScreen);
+
+	// detect fullscreen toggling
+	map.on('enterFullscreen', function(){
+		if(window.console) window.console.log('enterFullscreen');
+	});
+	map.on('exitFullscreen', function(){
+		if(window.console) window.console.log('exitFullscreen');
+	});
+
 });
